f57c591c06e5a8683c816090ecc3dce9b8c92b83
aabulhaj <aabulhaj1@gmail.com>
2019-02-18 21:44:27 +0200

    Solution 67

---
+++ solutions/solution67.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(require memo)
+
+(define triangle
+  (map (lambda (x)
+	 (map string->number (string-split x))) (file->lines "solution67-triangle.txt"))
+  )
+
+(define/memoize (max-path-sum lst i)
+  (cond
+    [(empty? lst) 0]
+    [else
+     (define curr-lst (car lst))
+     (max
+      (+ (list-ref curr-lst i) (max-path-sum (cdr lst) i))
+      (+ (list-ref curr-lst i) (max-path-sum (cdr lst) (add1 i)))
+      )]
+    )
+  )
+
+(max-path-sum triangle 0)
